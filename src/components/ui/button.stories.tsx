bf05871bb46317c0ef65ad8e039cb2c0bcde910f
mtosity <mtosity@gmail.com>
2025-06-12 22:53:56 -0400

    fix: unused icon

--- src/components/ui/button.stories.tsx
+++ src/components/ui/button.stories.tsx
@@ -26,12 +26,6 @@ const FlashlightIcon = () => (
   </svg>
 );
 
-const RewindIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-    <path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z" />
-  </svg>
-);
-
 const meta = {
   title: "UI/Button",
   component: Button,
